514e48b2eca46df39d5a5363996b6c36243c7480
Patrick Baker <pbaker@ipcove.com>
2014-08-20 22:01:50 -0400

    Add MongoDB support

---
+++ modules/mongodb/init.zsh
@@ -0,0 +1,16 @@
+#
+# Enable MongoDB·
+#
+# Authors:
+#   Patrick Baker <patricksbaker@gmail.com>
+#
+
+local mongod_path='/usr/local/bin/mongod'
+local mongod_pid='/usr/local/var/mongodb/mongod.lock'
+local mongo_log='/usr/local/var/log/mongodb/mongo.log'
+
+if [[ "$OSTYPE" == darwin* ]]; then
+    alias mongod-start="${mongod_path} --config /usr/local/etc/mongod.conf --fork"
+    alias mongod-stop='kill -15 `cat $mongod_pid`'
+    alias mongo-log="tail -f ${mongo_log}"
+fi
